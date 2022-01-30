b9ba59461a256d31c5e4ceafe34c50e45a2c60f7
ghorpadet847 <98678233+ghorpadet847@users.noreply.github.com>
2022-01-30 10:09:54 -0800

    basic data type

--- Day 1/myJavaProgram.java
+++ Day 1/myJavaProgram.java
@@ -1,1 +1,17 @@
-First java program
+//First java program
+
+#include<stdio.h>
+main()
+{
+  int a;
+  float b;
+  char ch;
+  
+  a = 4;
+  b = 5.8;
+  ch = 'm';
+  printf("\n%d", a);
+  printf("\n float value of b is %f",b);
+  printf("\n float value of ch is %c",ch);
+  return 0;
+}
